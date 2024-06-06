6baa41f362f35db1744a4106650afbdb83d37933
Félix SASSUS BOURDA <felix.sassus-bourda@proton.me>
2024-06-06 10:39:05 +0200

    Removed comments

--- .CoqMakefile.d
+++ .CoqMakefile.d
@@ -1,27 +1,21 @@
-theories/ContextLists.vo theories/ContextLists.glob theories/ContextLists.v.beautified theories/ContextLists.required_vo: theories/ContextLists.v theories/Types.vo
-theories/ContextLists.vio: theories/ContextLists.v theories/Types.vio
-theories/ContextLists.vos theories/ContextLists.vok theories/ContextLists.required_vos: theories/ContextLists.v theories/Types.vos
 theories/EvaluationSystem.vo theories/EvaluationSystem.glob theories/EvaluationSystem.v.beautified theories/EvaluationSystem.required_vo: theories/EvaluationSystem.v theories/LogicSet.vo
 theories/EvaluationSystem.vio: theories/EvaluationSystem.v theories/LogicSet.vio
 theories/EvaluationSystem.vos theories/EvaluationSystem.vok theories/EvaluationSystem.required_vos: theories/EvaluationSystem.v theories/LogicSet.vos
-theories/HeadEvaluation.vo theories/HeadEvaluation.glob theories/HeadEvaluation.v.beautified theories/HeadEvaluation.required_vo: theories/HeadEvaluation.v theories/EvaluationSystem.vo theories/Tactics.vo theories/LogicSet.vo theories/Types.vo
-theories/HeadEvaluation.vio: theories/HeadEvaluation.v theories/EvaluationSystem.vio theories/Tactics.vio theories/LogicSet.vio theories/Types.vio
-theories/HeadEvaluation.vos theories/HeadEvaluation.vok theories/HeadEvaluation.required_vos: theories/HeadEvaluation.v theories/EvaluationSystem.vos theories/Tactics.vos theories/LogicSet.vos theories/Types.vos
+theories/HeadEvaluation.vo theories/HeadEvaluation.glob theories/HeadEvaluation.v.beautified theories/HeadEvaluation.required_vo: theories/HeadEvaluation.v theories/EvaluationSystem.vo theories/Tactics.vo theories/LogicSet.vo theories/Syntax.vo theories/Types.vo
+theories/HeadEvaluation.vio: theories/HeadEvaluation.v theories/EvaluationSystem.vio theories/Tactics.vio theories/LogicSet.vio theories/Syntax.vio theories/Types.vio
+theories/HeadEvaluation.vos theories/HeadEvaluation.vok theories/HeadEvaluation.required_vos: theories/HeadEvaluation.v theories/EvaluationSystem.vos theories/Tactics.vos theories/LogicSet.vos theories/Syntax.vos theories/Types.vos
 theories/LogicSet.vo theories/LogicSet.glob theories/LogicSet.v.beautified theories/LogicSet.required_vo: theories/LogicSet.v 
 theories/LogicSet.vio: theories/LogicSet.v 
 theories/LogicSet.vos theories/LogicSet.vok theories/LogicSet.required_vos: theories/LogicSet.v 
 theories/Sets/Multiset.vo theories/Sets/Multiset.glob theories/Sets/Multiset.v.beautified theories/Sets/Multiset.required_vo: theories/Sets/Multiset.v 
 theories/Sets/Multiset.vio: theories/Sets/Multiset.v 
 theories/Sets/Multiset.vos theories/Sets/Multiset.vok theories/Sets/Multiset.required_vos: theories/Sets/Multiset.v 
+theories/Syntax.vo theories/Syntax.glob theories/Syntax.v.beautified theories/Syntax.required_vo: theories/Syntax.v 
+theories/Syntax.vio: theories/Syntax.v 
+theories/Syntax.vos theories/Syntax.vok theories/Syntax.required_vos: theories/Syntax.v 
 theories/Tactics.vo theories/Tactics.glob theories/Tactics.v.beautified theories/Tactics.required_vo: theories/Tactics.v 
 theories/Tactics.vio: theories/Tactics.v 
 theories/Tactics.vos theories/Tactics.vok theories/Tactics.required_vos: theories/Tactics.v 
-theories/Test.vo theories/Test.glob theories/Test.v.beautified theories/Test.required_vo: theories/Test.v 
-theories/Test.vio: theories/Test.v 
-theories/Test.vos theories/Test.vok theories/Test.required_vos: theories/Test.v 
-theories/TestFin.vo theories/TestFin.glob theories/TestFin.v.beautified theories/TestFin.required_vo: theories/TestFin.v 
-theories/TestFin.vio: theories/TestFin.v 
-theories/TestFin.vos theories/TestFin.vok theories/TestFin.required_vos: theories/TestFin.v 
 theories/Types.vo theories/Types.glob theories/Types.v.beautified theories/Types.required_vo: theories/Types.v theories/LogicSet.vo
 theories/Types.vio: theories/Types.v theories/LogicSet.vio
 theories/Types.vos theories/Types.vok theories/Types.required_vos: theories/Types.v theories/LogicSet.vos
--- theories/HeadEvaluation.v
+++ theories/HeadEvaluation.v
@@ -13,87 +13,8 @@ From Coq Require Import Unicode.Utf8.
 Require Import EvaluationSystem.
 Require Import Tactics.
 Require Import LogicSet.
-Module Syntax.
 
-
-
-Inductive term :Set :=
-  | Trm_Bound_Var : nat -> term
-  | Trm_App : term -> term -> term
-  | Trm_Abs : term -> term 
-.
-
-Declare Custom Entry term.
-Notation "<{ e }>" := e (e custom term at level 99).
-Notation "( x )" := x (in custom term, x at level 99).
-Notation "x" := x (in custom term at level 0, x constr at level 0).
-Notation "'#' n" := (Trm_Bound_Var n) (in custom term at level 0).
-Notation "x y" := (Trm_App x y) (in custom term at level 2, left associativity).
-Notation "'λ' t" := (Trm_Abs t) (
-  in custom term at level 90, 
-  t custom term at level 99,
-  left associativity
-).
-Notation "'`' e '`'" := e (in custom term at level 0, e constr).
-Search (∀ _, 0 < S _).
-
-Check <{ (λ #0 #1)(λ #1 #0) }>.
-
-
-Check le_gt_dec.
-Fixpoint shift (n : nat) (k : nat) (t : term) : term := 
-  match t with 
-  | <{ #p }> => 
-    match le_gt_dec p k with 
-    | left p_le_k => t
-    | right p_gt_k => <{ #`p + n - 1 ` }>
-    end
-  | <{ u v }> => <{`shift n k u` `shift n k v`}> 
-  | <{ λ u }> => <{ λ `shift n (k + 1) u` }>
-  end.
-
-Check lt_eq_lt_dec. 
-Reserved Notation "M '[' n '<-' N ']'" (
-  in custom term at level 0,
-  n constr
-).
-Fixpoint subst (t : term) (n : nat) (s : term) : term := 
-  match t with 
-  | <{ #m }> => 
-    match Nat.eq_dec n m with 
-    | left eq => shift n 0 s
-    | right neq => t
-    end
-  | <{ u v }> => <{(u[n <- s]) (v[n <- s])}>
-  | <{ λ u }> => <{λ u[n+1 <- s]}>
-  end
-  where
-  "M '[' n '<-' N ']'" := (subst M n N) (in custom term).
-
-Fixpoint lower (n : nat) (t : term) : term := 
-  match t with 
-  | <{#m}> =>
-    match le_gt_dec m n with 
-    | left m_le_n => t
-    | right m_gt_n => <{#`m-1`}>
-    end
-  | <{λ t}> => <{ λ `lower (n+1) t` }>
-  | <{t u}> => <{ `lower n t` `lower n u`}>
-  end.
-
-Compute <{ (λ #2 #0 #1)[0 <- #4] }>.
-
-
-Notation "t '[' s ']'" := (subst s t) (in custom term at level 1, s at level 99, no associativity).
-
-Check <{ λ λ ((λ #2 #1 #0) (λ #0))  }>.
-
-Compute <{(λ λ ((#2 #1 #0) [0 <- λ #0]))}>.
-Eval compute in <{(λ λ ((#2 #1 #0)[1 <- λ #0]))}>.
-Eval compute in <{(λ λ ((#2 #1 #0)))[0 <- λ #0]}>.
-
-End Syntax.
-Import Syntax.
+Require Import Syntax.
 
 
 
@@ -332,8 +253,6 @@ Section HeadTypingSystem.
 Ltac derivation_induction der P0 := 
   induction der using has_type_mut_ind with (P0:=P0); unfold P0 in *; clear P0.
 
-Locate "{ _ } + { _ }".
-
 Lemma permₘ_empty : ∀ mt, permutationₘ {{nil}} mt -> mt = {{nil}}.
 Proof with eauto with permutation_hints.
   intros.
@@ -341,12 +260,6 @@ Proof with eauto with permutation_hints.
   induction H; subst; try inversion Heqmt'...
   rewrite <- IHpermutationₘ1...
 Qed.
-
-Locate "∃".
-Check ex.
-
-
-
 
 Definition Id := <{ λ #0 }>.
 Definition example_term := <{ (λ ((λ #0 #1) #0)) `Id` }>.
@@ -422,6 +335,6 @@ Proof with eauto.
     + apply Ctx_union_tight in H1 as [H_tight_Γ₁ H_tight_Γ₂]...
       apply IH in H5; simpl in H5; try contradiction...
 
-Qed.
+Admitted.
 
 End HeadTypingSystem.
